fe5e9b0b01e1894c428ff54d2ea094d351f3ae33
Martinlindahltest <basmartin@gmail.com>
2020-02-19 22:19:52 +0100

    Home och detail screen i egna filer

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { View, Text, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import DetailsScreen from '../Screens/DetailsScreen'
+import HomeScreen from '../Screens/HomeScreen'
 
 
 
@@ -9,25 +11,25 @@ const MainNavigator = () => {
 
     const Stack = createStackNavigator();
 
-    function HomeScreen({ navigation }) {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => navigation.navigate('Details')}
-                />
-            </View>
-        );
-    }
+    // function HomeScreen({ navigation }) {
+    //     return (
+    //         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //             <Text>Home Screen</Text>
+    //             <Button
+    //                 title="Go to Details"
+    //                 onPress={() => navigation.navigate('Details')}
+    //             />
+    //         </View>
+    //     );
+    // }
 
-    function DetailsScreen() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
-            </View>
-        );
-    }
+    // function DetailsScreen() {
+    //     return (
+    //         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //             <Text>Details Screen</Text>
+    //         </View>
+    //     );
+    // }
 
     return (
         <NavigationContainer>
