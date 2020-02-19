8c0fc8a7bcf61a9764053abd39ec655ba89df408
Martinlindahltest <basmartin@gmail.com>
2020-02-19 20:16:30 +0100

    Screensmapp med filer

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 //import MealsNavigator from './Navigation/MealsNavigator'
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
