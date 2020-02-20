6b3ab49bb8653c03d199ab92587d72edd981417a
Martinlindahltest <basmartin@gmail.com>
2020-02-20 12:14:46 +0100

    mainnavigator innehåller både draw och stack

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -1,42 +1,42 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
+
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import DetailsScreen from '../Screens/DetailsScreen'
 import HomeScreen from '../Screens/HomeScreen'
+import { render } from 'react-dom';
+
 
 
 
 const MainNavigator = () => {
 
+    const Drawer = createDrawerNavigator();
     const Stack = createStackNavigator();
 
-    // function HomeScreen({ navigation }) {
-    //     return (
-    //         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    //             <Text>Home Screen</Text>
-    //             <Button
-    //                 title="Go to Details"
-    //                 onPress={() => navigation.navigate('Details')}
-    //             />
-    //         </View>
-    //     );
-    // }
-
-    // function DetailsScreen() {
-    //     return (
-    //         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    //             <Text>Details Screen</Text>
-    //         </View>
-    //     );
-    // }
-
-    return (
-        <NavigationContainer>
+    const StackNavigator = () => {
+        return (
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Details" component={DetailsScreen} />
             </Stack.Navigator>
+        )
+    }
+
+    return (
+
+        <NavigationContainer>
+            {/* <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator> */}
+            <Drawer.Navigator>
+                <Drawer.Screen name="Feed" component={StackNavigator} />
+                <Drawer.Screen name="Article" component={HomeScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
     )
 }
