1154d48515cb9aa0e0b832c46ee20821511adafa
Martinlindahltest <basmartin@gmail.com>
2020-02-25 10:15:45 +0100

    Drawer visar rätt

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -8,13 +8,21 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
-import { Ionicons } from '@expo/vector-icons';
 
 
 
 import DetailsScreen from '../Screens/DetailsScreen'
 import HomeScreen from '../Screens/HomeScreen'
-import iconSet from '@expo/vector-icons/build/FontAwesome5';
+
+import ForstaSida from '../Screens/ForstaSida'
+import Kalender from '../Screens/Kalender'
+import KalenderDetalj from '../Screens/KalenderDetalj'
+import Verksamheter from '../Screens/Verksamheter'
+import VerksamheterDetalj from '../Screens/VerksamheterDetalj'
+import Personal from '../Screens/Personal'
+import HittaHit from '../Screens/HittaHit'
+import OmAppen from '../Screens/OmAppen'
+
 
 const MainNavigator = () => {
 
@@ -50,6 +58,38 @@ const MainNavigator = () => {
         )
     }
 
+    const kalenderStack = () => {
+        return (
+            <Stack.Navigator initialRouteName="Kalender">
+                <Stack.Screen
+                    name="Kalender"
+                    component={Kalender}
+                    options={navData => {
+                        return {
+                            headerTitle: 'Kalender',
+                            headerLeft: () => {
+                                // console.log(navData)
+                                return <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                                    <Item
+                                        title="Menu"
+                                        iconName="ios-menu"
+                                        onPress={() => {
+                                            navData.navigation.toggleDrawer()
+                                        }}
+                                    />
+                                </HeaderButtons>
+                            }
+                        }
+                    }}
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={DetailsScreen}
+                />
+            </Stack.Navigator>
+        )
+    }
+
 
     return (
 
@@ -59,21 +99,40 @@ const MainNavigator = () => {
                     <View style={{ flex: 1, paddingTop: 20 }}>
                         <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
                             <DrawerItemList {...props} />
-                            <Button
+                            {/* <Button
                                 title="Logout"
                                 color={'blue'}
                                 onPress={() => {
                                     console.log('i drawer')
                                 }}
-                            />
+                            /> */}
                         </SafeAreaView>
                     </View>
                 );
             }}>
-                <Drawer.Screen name="Feed" component={HomeScreen} />
                 <Drawer.Screen
-                    name="med options"
-                    component={StackNavigator}
+                    name="Förstasida"
+                    component={ForstaSida}
+                />
+                <Drawer.Screen
+                    name="Kalender"
+                    component={kalenderStack}
+                />
+                <Drawer.Screen
+                    name="Versamheter"
+                    component={Verksamheter}
+                />
+                <Drawer.Screen
+                    name="Personal"
+                    component={Personal}
+                />
+                <Drawer.Screen
+                    name="Hitta hit"
+                    component={HittaHit}
+                />
+                <Drawer.Screen
+                    name="Om appen"
+                    component={OmAppen}
                 />
             </Drawer.Navigator>
         </NavigationContainer>
